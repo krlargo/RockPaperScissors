17b254e5ccea13eca89121ccd4fe8f325a033461
krlargo <krlargo@ucdavis.edu>
2017-12-11 21:15:48 -0800

    Start project

---
+++ RockPaperScissors/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RockPaperScissors
+//
+//  Created by Kevin Largo on 12/11/17.
+//  Copyright © 2017 Kevin Largo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
