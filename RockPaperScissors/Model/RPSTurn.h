6d7c0c56a0274a98e06639645658b93ca04b5955
krlargo <krlargo@ucdavis.edu>
2017-12-12 18:26:30 -0800

    Add RPSTurn

---
+++ RockPaperScissors/Model/RPSTurn.h
@@ -0,0 +1,13 @@
+//
+//  RPSTurn.h
+//  RockPaperScissors
+//
+//  Created by Kevin Largo on 12/12/17.
+//  Copyright © 2017 Kevin Largo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RPSTurn : NSObject
+
+@end
---
+++ RockPaperScissors/Model/RPSTurn.m
@@ -0,0 +1,13 @@
+//
+//  RPSTurn.m
+//  RockPaperScissors
+//
+//  Created by Kevin Largo on 12/12/17.
+//  Copyright © 2017 Kevin Largo. All rights reserved.
+//
+
+#import "RPSTurn.h"
+
+@implementation RPSTurn
+
+@end
