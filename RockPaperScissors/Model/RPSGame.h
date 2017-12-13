6b64a76177a6956aa39f732f59f800ee02aa9439
krlargo <krlargo@ucdavis.edu>
2017-12-12 18:25:17 -0800

    Add RPSGame

---
+++ RockPaperScissors/Model/RPSGame.h
@@ -0,0 +1,13 @@
+//
+//  RPSGame.h
+//  RockPaperScissors
+//
+//  Created by Kevin Largo on 12/12/17.
+//  Copyright © 2017 Kevin Largo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RPSGame : NSObject
+
+@end
---
+++ RockPaperScissors/Model/RPSGame.m
@@ -0,0 +1,13 @@
+//
+//  RPSGame.m
+//  RockPaperScissors
+//
+//  Created by Kevin Largo on 12/12/17.
+//  Copyright © 2017 Kevin Largo. All rights reserved.
+//
+
+#import "RPSGame.h"
+
+@implementation RPSGame
+
+@end
