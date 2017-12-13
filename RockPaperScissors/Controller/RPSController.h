fc1fa8e8320cd6e804e4d3f4155ba14fb26c7fe6
krlargo <krlargo@ucdavis.edu>
2017-12-12 18:51:58 -0800

    Add RPSController

---
+++ RockPaperScissors/Controller/RPSController.h
@@ -0,0 +1,15 @@
+//
+//  RPSController.h
+//  RockPaperScissors
+//
+//  Created by Kevin Largo on 12/12/17.
+//  Copyright © 2017 Kevin Largo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RPSController : NSObject
+
+-(void) throwDown: (Move) move;
+
+@end
---
+++ RockPaperScissors/Controller/RPSController.m
@@ -0,0 +1,17 @@
+//
+//  RPSController.m
+//  RockPaperScissors
+//
+//  Created by Kevin Largo on 12/12/17.
+//  Copyright © 2017 Kevin Largo. All rights reserved.
+//
+
+#import "RPSController.h"
+
+@implementation RPSController
+
+-(void) throwDown: (Move) move {
+    
+}
+
+@end
